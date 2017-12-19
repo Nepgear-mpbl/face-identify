6a091d5ba186df3daa8b4484243d10faa9aab769
Rain <364687927@qq.com>
2017-12-19 10:25:53 +0800

    添加opencv截取人脸

---
+++ pre_handle.py
@@ -0,0 +1,26 @@
+import cv2
+import os.path
+
+
+def detect(filename, cascade_file="haarcascade_frontalface_alt.xml"):
+    if not os.path.isfile(cascade_file):
+        raise RuntimeError("%s: not found" % cascade_file)
+
+    cascade = cv2.CascadeClassifier(cascade_file)
+    image = cv2.imread(filename)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+
+    faces = cascade.detectMultiScale(gray,
+                                     # detector options
+                                     scaleFactor=1.1,
+                                     minNeighbors=3,
+                                     minSize=(47, 55))
+    for i, (x, y, w, h) in enumerate(faces):
+        face = image[y: y + h, x:x + w, :]
+        face = cv2.resize(face, (47, 55))
+        return face
+
+
+
--- predict.py
+++ predict.py
@@ -1,20 +1,8 @@
 import tensorflow as tf
 import pickle
-from PIL import Image
 import numpy as np
-from scipy.spatial.distance import cosine
+from pre_handle import detect
 
-
-def crop_img_by_half_center(src_file_path,dest_file_path):
-    im = Image.open(src_file_path)
-    x_size, y_size = im.size
-    start_point_xy = x_size / 4
-    end_point_xy = x_size / 4 + x_size / 2
-    box = (start_point_xy, start_point_xy, end_point_xy, end_point_xy)
-    new_im = im.crop(box)
-    new_new_im = new_im.resize((47, 55))
-    new_new_im.save(dest_file_path)
-    return new_new_im
 
 def Verify(A, G, x1, x2):
     x1.shape = (-1, 1)
@@ -31,37 +19,41 @@ def load_jbdata():
         return testX1, testX2,testY
 
 if __name__ == '__main__':
-    # pic1 = crop_img_by_half_center('testpic/test1.jpg','testpic/test1c.jpg')
-    # pic2 = crop_img_by_half_center('testpic/test2.jpg','testpic/test2c.jpg')
-    # pic1arr = np.reshape(np.asarray(pic1, dtype='float32'), [1, 55, 47, 3])
-    # pic2arr = np.reshape(np.asarray(pic2, dtype='float32'), [1, 55, 47, 3])
-    testX1, testX2, label = load_jbdata()
+    pic1 = detect('testpic/test1.jpg')
+    pic2 = detect('testpic/test2.jpg')
+    pic1arr = np.reshape(np.asarray(pic1, dtype='float32'), [1, 55, 47, 3])
+    pic2arr = np.reshape(np.asarray(pic2, dtype='float32'), [1, 55, 47, 3])
+    # testX1, testX2, label = load_jbdata()
 
     saver = tf.train.import_meta_graph("checkpoint/25000.ckpt.meta")
     with tf.Session() as sess:
         input = tf.get_default_graph().get_tensor_by_name('input/x:0')
         validate = tf.get_default_graph().get_tensor_by_name('DeepID1/validate:0')
         saver.restore(sess, 'checkpoint/25000.ckpt')
-        h1 = sess.run(validate, {input: testX1})
-        h2 = sess.run(validate, {input: testX2})
+        h1 = sess.run(validate, {input: pic1arr})
+        h2 = sess.run(validate, {input: pic2arr})
     with open('JointBayesian_Model/A.pkl', 'rb') as f:
         A = pickle.load(f)
     with open('JointBayesian_Model/G.pkl', 'rb') as f:
         G = pickle.load(f)
-    # pre_y = np.array([cosine(x, y) for x, y in zip(h1, h2)])
-    # print(pre_y)
-    predict=np.zeros([len(label)])
-    print(len(label))
-    count=0
-    for i in range(len(label)):
-        result = Verify(A, G, h1[i], h2[i])
-        if result>=-17.65:
-            predict[i]=1
-        else:
-            predict[i]=0
-        if predict[i]==label[i]:
-            count+=1
-    correct_rate=float(count/len(label))
-    print(count)
-    print(correct_rate)
+    result = Verify(A, G, h1, h2)
+    if result>=-17.65:
+        print(True)
+    else:
+        print(False)
+
+    # predict=np.zeros([len(label)])
+    # print(len(label))
+    # count=0
+    # for i in range(len(label)):
+    #     result = Verify(A, G, h1[i], h2[i])
+    #     if result>=-17.65:
+    #         predict[i]=1
+    #     else:
+    #         predict[i]=0
+    #     if predict[i]==label[i]:
+    #         count+=1
+    # correct_rate=float(count/len(label))
+    # print(count)
+    # print(correct_rate)
 
